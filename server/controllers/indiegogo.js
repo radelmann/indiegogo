48d932f8d10ece44c8d5baac2154bce6348b5cd0
radelmann <robadelmann@gmail.com>
2016-03-16 16:13:29 -0700

    get api route for indiegogo data

--- server/controllers/indiegogo.js
+++ server/controllers/indiegogo.js
@@ -1,8 +1,24 @@
+var https = require('https');
+
 module.exports = {
   get: function(req, res, next) {
-    res.status(200);
-    res.json({
-      "success": "OK"
+
+    https.get('https://api.indiegogo.com/1/campaigns.json?api_token=e377270bf1e9121da34cb6dff0e8af52a03296766a8e955c19f62f593651b346', function(httpResponse) {
+      var resData = '';
+
+      httpResponse.on('data', function(chunk) {
+        resData += chunk;
+      });
+
+      httpResponse.on('end', function() {
+        res.status(200);
+        res.json(resData);
+      });
+
+      httpResponse.on('error', function(err) {
+        res.status(err.statusCode || 500);
+        res.json(err);
+      });
     });
   }
 }

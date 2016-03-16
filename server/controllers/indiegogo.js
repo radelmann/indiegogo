4f8cf5e1fc505b7b3d30c4a38dbcd4bc0d1ab798
radelmann <robadelmann@gmail.com>
2016-03-16 16:22:48 -0700

    code cleanup

--- server/controllers/indiegogo.js
+++ server/controllers/indiegogo.js
@@ -1,9 +1,10 @@
 var https = require('https');
+var url = "https://api.indiegogo.com/1/campaigns.json?api_token=e377270bf1e9121da34cb6dff0e8af52a03296766a8e955c19f62f593651b346";
 
 module.exports = {
   get: function(req, res, next) {
 
-    https.get('https://api.indiegogo.com/1/campaigns.json?api_token=e377270bf1e9121da34cb6dff0e8af52a03296766a8e955c19f62f593651b346', function(httpResponse) {
+    https.get(url, function(httpResponse) {
       var resData = '';
 
       httpResponse.on('data', function(chunk) {

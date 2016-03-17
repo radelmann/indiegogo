a955247c270bc2e25e4ef0afba9ef5461147aa5c
radelmann <robadelmann@gmail.com>
2016-03-16 22:54:45 -0700

    fix angular dependency injection to work with minified code

--- client/app/api/api.js
+++ client/app/api/api.js
@@ -1,9 +1,9 @@
 angular.module('indiegogo.api', [])
-  .controller('apiController', function($scope, API) {
+  .controller('apiController', ['$scope','API', function($scope, API) {
     $scope.data = {};
     $scope.data.items = [];
 
     API.get().then(function(data) {
       $scope.data.items = data;
     });
-  });
+  }]);
